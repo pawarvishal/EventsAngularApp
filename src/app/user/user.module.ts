fc55309d950d27a2fc8dbca3d8c6b2353e298b4e
Vishal Pawar <openstack.vishal@gmail.com>
2021-05-23 23:52:57 +0530

    created template based form with basic oneway binding

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { LoginComponent } from "./login.component";
 import { ProfileComponent } from "./profile.component";
 
 import { userRoutes } from './user.routes';
@@ -9,11 +11,12 @@ import { userRoutes } from './user.routes';
 @NgModule({
     imports: [
         CommonModule,
-        RouterModule.forChild(userRoutes)
-
+        RouterModule.forChild(userRoutes),
+        FormsModule
     ],
     declarations: [
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent
     ],
     providers:[]
 
